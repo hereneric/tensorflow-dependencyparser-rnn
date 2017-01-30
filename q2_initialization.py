ed5e9556b27fccd3cecda3bd4f40cc36c5284c9b
Eric-H-Ren <rhilogin@gmail.com>
2017-01-29 21:29:04 -0800

    q2 (e) done delete comment

--- q2_initialization.py
+++ q2_initialization.py
@@ -24,8 +24,6 @@ def xavier_weight_init():
         Returns:
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
-        ### YOUR CODE HERE
-        ### END YOUR CODE
         shape=list(shape)
         epsilon = np.sqrt(6) / np.sum(shape)
         out = tf.random_uniform(shape, minval=-epsilon, maxval=epsilon)
